24ced8bd73f677bcda0b7652659bd01b495b3eca
Shailyn-Ramsamy <shailyn.ramsamy@gmail.com>
2023-08-24 13:38:18 +0200

    Splash Screen implementation

--- Screens/Splash.js
+++ Screens/Splash.js
@@ -28,7 +28,6 @@ export default function SplashScreen({navigation}) {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: 'white',
         alignItems: 'center',
         justifyContent: 'center',
