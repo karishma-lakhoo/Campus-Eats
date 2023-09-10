899fde28f1f271c27c0e7c341715d2bdd5d4c305
karishma <karishma@lakhoo.net>
2023-09-10 14:30:49 +0200

    WIP search bar

--- Screens/Restaurants.js
+++ Screens/Restaurants.js
@@ -1,15 +1,26 @@
 import React, { useEffect, useState } from "react";
-import {Dimensions, FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image} from "react-native";
+import {
+    Dimensions,
+    FlatList,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    Image,
+    TextInput, Button
+} from "react-native";
 import * as Font from "expo-font";
-import Colors from "../colors";
 import restaurants from '../consts/dummyRestaurants'
+import Icon from "react-native-vector-icons/MaterialIcons";
+
 
 const { width, height } = Dimensions.get("window");
 
 const RestaurantsScreen = () => {
     const [fontLoaded, setFontLoaded] = useState(false);
-
-
+    const [searchText, setSearchText] = useState("");
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
 
     useEffect(() => {
         async function loadFont() {
@@ -22,18 +33,52 @@ const RestaurantsScreen = () => {
         loadFont();
     }, []);
 
-    if (!fontLoaded) {
-        return null;
-    }
+    useEffect(() => {
+        // Filter the restaurants based on the search text
+        const filtered = restaurants.filter(item => item.name.toLowerCase().includes(searchText.toLowerCase()));
+        setFilteredRestaurants(filtered);
+    }, [searchText]);
+
+    const handleSearchTextChange = (text) => {
+        setSearchText(text);
+    };
+
+    const handleFilterPress = () => {
+        // Add your filter logic here
+    };
 
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.header}>
                 <Text style={[styles.heading, styles.heading]}>Restaurants</Text>
             </View>
-            <View style={{ marginTop: height * 0.15 ,marginHorizontal: 10,}}>
+
+            <View style={{ marginHorizontal: 10}}>
+                <View style={styles.searchContainer}>
+                    <Image
+                        source={require('../assets/Search.png')}
+                        style={{ width: 24, height: 24 }}
+                        onPress={() => {handleFilterPress()}}
+                    />
+
+                    <TextInput
+                        style={styles.searchInput}
+                        placeholder="Search..."
+                        value={searchText}
+                        onChangeText={handleSearchTextChange}
+                    />
+                    <View style={styles.filter}>
+                        <TouchableOpacity onPress={() => { handleFilterPress() }} style={styles.filter}>
+                            <Image
+                                source={require('../assets/Filter.png')}
+                                style={{ width: 24, height: 24 }}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
                 <FlatList
-                    data={restaurants}
+                    data={filteredRestaurants}
                     keyExtractor={item => item.key}
                     renderItem={({ item }) => (
                         <TouchableOpacity style={styles.itemContainer}>
@@ -95,6 +140,27 @@ const styles = StyleSheet.create({
 
     flatListContainer:{
         padding: 10
+    },
+    searchInput: {
+        flex: 1,
+        borderColor: "gray",
+        marginRight: 10,
+        padding: 5,
+    },
+    searchContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginTop: height*0.08,
+        marginHorizontal: 10,
+        backgroundColor: "white",
+        borderRadius: 5,
+        padding: 10
+
+
+    },
+    search: {
+        position: "absolute"
     }
 });
 
