00300462901e921a3196202cffac7fc4fc121b47
Shailyn-Ramsamy <shailyn.ramsamy@gmail.com>
2023-09-20 23:28:48 +0200

    added profile page popup

---
+++ PopUps/PFPpopup.js
@@ -0,0 +1,83 @@
+import {
+    Modal,
+    Dimensions,
+    FlatList,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    TouchableWithoutFeedback
+} from "react-native";
+import React from 'react'
+const deviceHeight = Dimensions.get("window").height
+
+export class PFPpopup extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false
+        }
+    }
+
+    show = () => {
+        this.setState({show: true})
+    }
+
+    close = () => {
+        this.setState({show: false})
+    }
+
+    renderOutsideTouchable(onTouch){
+        const view = <View style={{flex: 1, width: '100%'}}/>
+        if (!onTouch) return view
+
+        return(
+            <TouchableWithoutFeedback onPress={onTouch} style={{flex: 1, width: '100%'}}>
+                {view}
+            </TouchableWithoutFeedback>
+        )
+    }
+
+    render() {
+        let {show} = this.state
+        const {onTouchOutside, title} = this.props
+
+        return(
+            <Modal
+                animationType={"fade"}
+                transparent={true}
+                visible={show}
+                onRequestClose={this.close}
+            >
+                <View style={{
+                    flex: 1,
+                    backgroundColor: "#000000AA",
+                    justifyContent: 'flex-end'
+                }}>
+                    {this.renderOutsideTouchable(onTouchOutside)}
+                    <View style={{
+                        backgroundColor: '#FFFFFF',
+                        width: '100%',
+                        borderTopLeftRadius:10,
+                        borderTopRightRadius:10,
+                        paddingHorizontal: 10,
+                        maxHeight: deviceHeight*0.4
+                    }}>
+                        <View>
+                            <Text style={{
+                                color: '182E44',
+                                fontSize: 20,
+                                fontWeight: '500',
+                                margin: 15,
+                            }}>
+                                {title}
+                            </Text>
+                        </View>
+
+                    </View>
+                </View>
+            </Modal>
+        )
+    }
+}
