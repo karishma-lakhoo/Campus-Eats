4fac7e1879a782ffa3ab8971b27120a6f165d308
Thembalethu Thwala <2321610@students.wits.ac.za>
2023-10-19 23:07:23 +0200

    add to cart added

--- consts/cartData.js
+++ consts/cartData.js
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from 'react';
 
-import {collection,getFirestore, doc, getDocs} from 'firebase/firestore';
+import {collection,getFirestore, doc, getDocs, updateDoc, arrayUnion} from 'firebase/firestore';
 import { getAuth} from "firebase/auth";
 
 
@@ -41,4 +41,34 @@ export function getCart(){
         getCartData();
     }, []);
     return [foodIDs, loading];
+}
+
+export async function addToCart(foodId){
+    const auth = getAuth();
+    const currentUser = auth.currentUser;
+    let userUID;
+    
+    if(currentUser){
+        userUID = currentUser.uid;
+    }
+    else{
+        console.log("User not logged in!");
+    }
+
+    try {
+        const usersCollection = collection(db, 'users');
+        const userDoc = doc(usersCollection,userUID);
+        const cartCollection = collection(userDoc,'Cart');
+        const querySnapshot = await getDocs(cartCollection);
+
+        querySnapshot.forEach(async (document) => {
+            const docId = document.id;
+    
+            await updateDoc(doc(cartCollection, docId),{
+                foodIDs: arrayUnion(...[foodId]),
+                });
+          });
+    } catch (error) {
+        console.log('Error getting documents', error);
+    }
 }
